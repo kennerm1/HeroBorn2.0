4943b6bee061dba99309d98dc5d46d49ad7b360a
kennerm1 <123021312+kennerm1@users.noreply.github.com>
2023-03-05 15:36:50 -0700

    Minimap Retry

--- HeroBornRedo/Assets/HeroBorn/Scripts/GameBehavior.cs
+++ HeroBornRedo/Assets/HeroBorn/Scripts/GameBehavior.cs
@@ -7,6 +7,7 @@ using CustomExtensions;
 
 public class GameBehavior : MonoBehaviour, IManager
 {
+    public Stack<string> lootStack = new Stack<string>();
     private string _state;
 
     public string State
@@ -18,6 +19,8 @@ public class GameBehavior : MonoBehaviour, IManager
     public string labelText = "Collect all 4 items and win your freedom!";
     public int maxItems = 4;
     public bool showLossScreen = false;
+    public delegate void DebugDelegate(string newText);
+    public DebugDelegate debug = Print;
 
     private int _itemsCollected = 0;
     public int Items
@@ -69,13 +72,36 @@ public class GameBehavior : MonoBehaviour, IManager
     void Start()
     {
         Initialize();
+        InventoryList<string> inventoryList = new
+            InventoryList<string>();
+        inventoryList.SetItem("Potion");
+        Debug.Log(inventoryList.item);
     }
 
     public void Initialize()
     {
         _state = "Manager initialized..";
         _state.FancyDebug();
-        Debug.Log(_state);
+        debug(_state);
+        LogWithDelegate(debug);
+        GameObject player = GameObject.Find("Player");
+        PlayerBehavior playerBehavior = player.GetComponent<PlayerBehavior>();
+        playerBehavior.playerJump += HandlePlayerJump;
+    }
+
+    public void HandlePlayerJump()
+    {
+        debug("Player has jumped...");
+    }
+
+    public static void Print(string newText)
+    {
+        Debug.Log(newText);
+    }
+
+    public void LogWithDelegate(DebugDelegate del)
+    {
+        del("Delegating the debug task...");
     }
 
     void OnGUI()
@@ -88,15 +114,37 @@ public class GameBehavior : MonoBehaviour, IManager
         {
             if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 50, 200, 200), "YOU WON!"))
             {
-                Utilities.RestartLevel(0);
+                RestartLevel();
             }
         }
         if (showLossScreen)
         {
             if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 50, 200, 100), "You lose..."))
             {
-                Utilities.RestartLevel();
+                try
+                {
+                    Utilities.RestartLevel(-1);
+                    debug("Level restarted succesfully...");
+                }
+                catch (System.ArgumentException e)
+                {
+                    Utilities.RestartLevel(0);
+                    debug("Reverting to scene 0: " + e.ToString());
+                }
+                finally
+                {
+                    debug("Restart handled...");
+                }
+
             }
         }
+    }
+
+    public void PrintLootReport()
+    {
+        var currentItem = lootStack.Pop();
+        var nextItem = lootStack.Peek();
+        Debug.LogFormat("You got a {0}! You've got a good chance of findng a {1} next!", currentItem, nextItem);
+        Debug.LogFormat("There are {0} random loot items waiting for you!", lootStack.Count);
     }
 }
